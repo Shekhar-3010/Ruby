c18ef2f0341e6808daa03983d9a63fba967e2cbd
Shekhar-3010 <98082889+Shekhar-3010@users.noreply.github.com>
2023-03-10 21:44:45 +0530

    first day of ruby

---
+++ hello.rb
@@ -0,0 +1,27 @@
+# a=1;
+# b=2;
+# c=a+b;
+# puts "the sum of #{a}+#{b} =#{c}"
+
+
+# puts "first line"
+# puts "second line"
+
+# print "first line"
+# print "second line"
+
+
+# def add(a,b)
+#     return a+b
+# end 
+# print "#{add(2,3)} \n"
+# print add(4,5)
+
+# def swap(a,b)
+#     puts "before swapping #{a} and #{b}"
+#     a,b=b,a
+#     puts "After swapping #{a} and #{b}"
+# end
+
+swap(5,4)
+
