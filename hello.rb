8fc700e377c2dcd94c8ff65d46d3f5efb1122923
Shekhar-3010 <98082889+Shekhar-3010@users.noreply.github.com>
2023-03-11 16:48:25 +0530

    ADD:arrays

--- hello.rb
+++ hello.rb
@@ -1,27 +1,29 @@
-# a=1;
-# b=2;
-# c=a+b;
-# puts "the sum of #{a}+#{b} =#{c}"
+#variables and print statements 
+a=1;
+b=2;
+c=a+b;
+puts "the sum of #{a}+#{b} =#{c}"
 
 
-# puts "first line"
-# puts "second line"
+puts "first line"
+puts "second line"
 
-# print "first line"
-# print "second line"
+print "first line"
+print "second line"
 
+#functions
+def add(a,b)
+    return a+b
+end 
+print "#{add(2,3)} \n"
+print add(4,5)
 
-# def add(a,b)
-#     return a+b
-# end 
-# print "#{add(2,3)} \n"
-# print add(4,5)
-
-# def swap(a,b)
-#     puts "before swapping #{a} and #{b}"
-#     a,b=b,a
-#     puts "After swapping #{a} and #{b}"
-# end
+#swapping function in ruby
+def swap(a,b)
+    puts "before swapping #{a} and #{b}"
+    a,b=b,a
+    puts "After swapping #{a} and #{b}"
+end
 
 swap(5,4)
 
